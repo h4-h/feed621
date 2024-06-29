f280151e459a7bd36f018ba0594b7b9a13ce267c
hash <maksim23090421@gmail.com>
2024-06-29 07:16:22 +0700

    Simplify models

--- services/backend/src/models/subscription_models.rs
+++ services/backend/src/models/subscription_models.rs
@@ -4,7 +4,7 @@ use utoipa::ToSchema;
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
 #[schema(
     title = "Subscription",
-    description = "Full subscription model.",
+    description = "Subscription model.",
 )]
 pub(crate) struct Subscription {
     #[schema(example = 682167858)]
@@ -13,16 +13,4 @@ pub(crate) struct Subscription {
     pub topic_id: i64,
     #[schema(example = 13371337)]
     pub last_post_id: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize, ToSchema)]
-#[schema(
-    title = "New subscription",
-    description = "Subscription model, used in create requests.",
-)]
-pub(crate) struct NewSubscription {
-    #[schema(example = 682167858)]
-    pub user_id: i64,
-    #[schema(example = 1)]
-    pub topic_id: i64,
 }
