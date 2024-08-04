bdf164de0682ad0783c59f3b41e271bc38c1f97b
hash <maksim23090421@gmail.com>
2024-08-04 22:34:52 +0700

    Update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,20 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
+if [[ ! -d "./docker" ]]; then
+  printf "${RED}Use this command from the root of the project${NC}\n"
+  exit -1
+fi
+
 if [[ "$1" != "dev" && "$1" != "prod" ]]; then
   printf "${RED}Usage: start.sh <dev | prod>${NC}\n"
   exit -1
 fi
 
-PATH_PREFIX="."
-[ -d "./scripts" ] || ".."
+BASE_COMPOSE="./docker/docker-compose.yaml"
+DEV_COMPOSE="./docker/docker-compose.dev.yaml"
+PROD_COMPOSE="./docker/docker-compose.prod.yaml"
+ENV_FILE="./.env"
 
-BASE_COMPOSE="${PATH_PREFIX}/docker-compose.yaml"
-DEV_COMPOSE="${PATH_PREFIX}/docker-compose.dev.yaml"
-PROD_COMPOSE="${PATH_PREFIX}/docker-compose.prod.yaml"
-
-[ "$1" == "dev" ] && docker compose -f ${BASE_COMPOSE} -f ${DEV_COMPOSE} up -w
-[ "$1" == "prod" ] && docker compose -f ${BASE_COMPOSE} -f ${PROD_COMPOSE} up -d
+[ "$1" == "dev" ] && docker compose --env-file ${ENV_FILE} -f ${BASE_COMPOSE} -f ${DEV_COMPOSE} up -w
+[ "$1" == "prod" ] && docker compose --env-file ${ENV_FILE} -f ${BASE_COMPOSE} -f ${PROD_COMPOSE} up -d
